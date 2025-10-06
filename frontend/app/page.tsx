537857f471f29c9b3d27e6b5bfdf008e86a5dd29
PeytonWilson <huongquetahuu@gmail.com>
2025-10-06 15:04:36 +0800

    homepage basic layout [skip ci]

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import "./polyfills.client";
+import { FhevmProvider } from "../components/providers";
+import { App } from "../components/App";
+
+export default function Page() {
+  return (
+    <FhevmProvider>
+      <App />
+    </FhevmProvider>
+  );
+}
+
+
